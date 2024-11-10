919a5ea48827db119b055ac50960afe7b1b83f02
Michelle Di Guglielmo <mfdigug@gmail.com>
2024-11-10 22:04:33 +1100

    done with javascript events acting on events lab

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+    }
+});
+
+/*  You'll need to add another condition to your event listener's callback function to call moveDodgerRight(). Then, inside the function, instead of moving the dodger ${left - 1}px, you'll be moving it ${left + 1}px.*/
+
+//Need to update code to stop dodger leaving the black game box
+//dodger is 40px wide
+
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left < 360) {
+        dodger.style.left = `${left + 1}px`;
+    };
+};
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+});
+
+
